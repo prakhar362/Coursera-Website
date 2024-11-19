ff8e38969e2a5e26028f93424b585494ccd39de4
prakhar362 <prakharshri2005@gmail.com>
2024-11-19 20:14:39 +0530

    step 6: added database schema and connected to cloud

--- db.js
+++ db.js
@@ -1,27 +1,45 @@
-// create the schema from readme file.
-const mongoose=require('mongoose');
-const {Schema,default:mongoose}=require("mongoose");
+const mongoose = require("mongoose");
+console.log("conneted to MongoDb Server")
+mongoose.connect("mongodb+srv://Ps100xDevs:aQPHHTqhFOnkGTB9@cluster0.4rpwgtp.mongodb.net/coursera-app")
 
-const userSchema=Schema({
-   
-})
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
-const adminSchema=Schema({
-   
-})
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
 
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
 
-const courseSchema=Schema({
-   
-})
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
 
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
 
-const purchaseSchema=Schema({
-   
-})
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
 
-const userModel=mongoose.Model("user",userSchema);
-const adminModel=mongoose.Model("admin",userSchema);
-const courseModel=mongoose.Model("course",userSchema);
-const purchaseModel=mongoose.Model("purchase",userSchema);
-
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
