c575f3c654dd2bbfaf8386bdf0073dd464a80f18
prakhar362 <prakharshri2005@gmail.com>
2024-11-29 09:28:54 +0530

    step 13:courses endpoint done for user

--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,40 @@
 const { Router } = require("express");
+const { userMiddlware } = require("../middlewares/user");
+const { purchaseModel, courseModel } = require("../db");
 const courseRouter = Router();
 
-courseRouter.post("/course/purchase", function(req, res) {
+courseRouter.post("/purchase",userMiddlware,async function(req, res) {
     // you would expect the user to pay you money
+    const userId=req.userId;
+    const courseId=req.body.courseId;
+
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
+    res.json({
+        message: "you have successfully purchased content"
+    })
+})
+
+courseRouter.get("/preview", async function(req, res) {
+    const courses=await courseModel.find({});
     res.json({
         message: "signup endpoint"
     })
 })
 
-courseRouter.get("/courses", function(req, res) {
+courseRouter.get("/purchases",userMiddlware,async function(req, res) {
+    
+    const userId=req.userId;
+    const purchases=
+    await purchaseModel.find({
+        userId,
+    });
+
     res.json({
-        message: "signup endpoint"
+       purchases
     })
 })
 
